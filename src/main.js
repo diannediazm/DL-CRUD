3aa65a04ed72662648acc64f1accb771bd9ef288
Dianne <dianne.diazm@gmail.com>
2020-12-08 18:54:03 -0300

    primer commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router/router.js';
+import store from './store/router.js';
+import firebase from 'firebase';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import { firebaseConfig } from "./config/firebaseConfig";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+firebase.initializeApp(firebaseConfig);
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
